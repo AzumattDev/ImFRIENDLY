6590ac68ba4cdd6b27f96d68dfff22a4dee97f02
AzumattDev <azumattgaming@gmail.com>
2022-11-26 07:45:05 -0500

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+
+namespace ImFRIENDLY
+{
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class ImFRIENDLYDAMMITPlugin : BaseUnityPlugin
+    {
+        internal const string ModName = "ImFRIENDLYDAMMIT";
+        internal const string ModVersion = "1.0.0";
+        internal const string Author = "Azumatt";
+        private const string ModGUID = Author + "." + ModName;
+
+        private readonly Harmony _harmony = new(ModGUID);
+
+        public static readonly ManualLogSource ImFRIENDLYDAMMITLogger =
+            BepInEx.Logging.Logger.CreateLogSource(ModName);
+
+        public void Awake()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            _harmony.PatchAll(assembly);
+        }
+    }
+
+    [HarmonyPatch(typeof(Turret), nameof(Turret.UpdateTarget))]
+    static class TurretUpdateTargetPatch
+    {
+        static void Prefix(Turret __instance)
+        {
+            ImFRIENDLYDAMMIT(__instance);
+        }
+
+        static void Postfix(Turret __instance)
+        {
+            ImFRIENDLYDAMMIT(__instance);
+        }
+
+        private static void ImFRIENDLYDAMMIT(Turret __instance)
+        {
+            if (__instance.m_target == null) return;
+            if (__instance.m_target.IsPlayer() || __instance.m_target.IsTamed() || __instance.m_target == Player.m_localPlayer)
+            {
+                __instance.m_target = null;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,25 @@
+# Description
+
+## Tired of getting hit by your own ballista? Me fuckin too. Protects tames as well. Enjoy.
+
+`This mod is not needed on a server. It's client only. Install on each client that you wish to have the mod load.`
+
+`Feel free to reach out to me on discord if you need manual download assistance.`
+
+# Author Information
+
+### Azumatt
+
+`DISCORD:` Azumatt#2625
+
+`STEAM:` https://steamcommunity.com/id/azumatt/
+
+For Questions or Comments, find me in the Odin Plus Team Discord or in mine:
+
+[![https://i.imgur.com/XXP6HCU.png](https://i.imgur.com/XXP6HCU.png)](https://discord.gg/Pb6bVMnFb2)
+<a href="https://discord.gg/pdHgy6Bsng"><img src="https://i.imgur.com/Xlcbmm9.png" href="https://discord.gg/pdHgy6Bsng" width="175" height="175"></a>
+***
+
+> # Update Information (Latest listed first)
+> ### v1.0.0
+> - Initial Release
